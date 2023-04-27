f11ad319145c712d2f08f671855a0e0e2caee1f8
Cinthia-Condori <131977205+Cinthia-Condori@users.noreply.github.com>
2023-04-27 09:12:33 -0500

    proyecto1

---
+++ Persona.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Vehiculo.h"
+using namespace System;
+public ref class Persona{
+	//Atributos
+public:
+	Vehiculo Vehiculo;
+	String^ Nombre;
+	String^ ApPaterno;
+	String^ ApMaterno;
+	String^ DNI;
+	String^ Edad;
+	String^ Genero;
+	String^ Nro_Cel;
+	String^ Direccion;
+};
+
---
+++ Vehiculo.h
@@ -0,0 +1,13 @@
+#pragma once
+using namespace System;
+public ref class Vehiculo{
+public:
+	//Atributos
+	String^ Tipo;
+	String^ color;
+	String^ marca;
+	String^ modelo;
+	String^ anio;
+	String^ precio;
+};
+
